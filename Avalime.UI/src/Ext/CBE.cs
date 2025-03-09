c653f2ba0dd6981389501df322684371e4d4bc3b
Tsinswreng <2290019838@qq.com>
2025-03-09 21:11:49 +0800

    Update CBE.cs

--- Avalime.UI/src/Ext/CBE.cs
+++ Avalime.UI/src/Ext/CBE.cs
@@ -1,54 +1,112 @@
+//2025-03-09T21:11:06.192+08:00_W10-7
 using System;
 using System.Linq.Expressions;
-using System.Reactive.Linq;
-using Avalime.UI.Ext;
-using Avalime.ViewModels.key;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Data;
 using Avalonia.Data.Core;
-using Avalonia.Interactivity;
-using Avalonia.Layout;
 using Avalonia.Markup.Xaml.MarkupExtensions;
 using Avalonia.Markup.Xaml.MarkupExtensions.CompiledBindings;
-using Avalonia.Media;
-using Avalonia.Styling;
 namespace Shr.Avalonia;
+
+/// <summary>
+//于c#中用編譯期綁定
+// usage:
+// using Ctx = MyDataContext;
+//,new Binding(nameof(ctx.hasValue)) ->
+//,new CBE(CBE.pth<Ctx, bool>(x=>x.hasValue))
+//正則替換:
+//new Binding(nameof(ctx\.(.*?)))
+//new CBE(CBE.pth<Ctx, object?>(x=>x.$1))
+//
+/// </summary>
 public class CBE : CompiledBindingExtension{
 	public CBE(CompiledBindingPath path):base(path){}
 
-	// public CBE(Func<CBE, CompiledBindingPath> f):base(f(this)){
+	public static CompiledBindingPath pth<T>(
+		Expression<Func<T, object?>> propertySelector
+	){
+		return pth<T, object?>(propertySelector);
+	}
 
-	// }
 
-
-	// public CompiledBindingPath cpth2<T, Tar>(
-	// 	Expression<Func<T, string>> propertySelector
-	// ){
-	// 	return cpth<T, Tar>(propertySelector);
-	// }
 
 	public static CompiledBindingPath pth<T, Tar>(
-		Expression<Func<T, string>> propertySelector
-	){
+		Expression<Func<T, Tar>> propertySelector)
+	{
 		var builder = new CompiledBindingPathBuilder();
-		var memberExpr = (MemberExpression)propertySelector.Body;
-		var propName = memberExpr.Member.Name;
+		var body = propertySelector.Body;
+
+		// 处理类型转换表达式（如值类型装箱）
+		if (body is UnaryExpression { NodeType: ExpressionType.Convert } unaryExpr)
+			body = unaryExpr.Operand;
+
+		switch (body)
+		{
+			case MemberExpression memberExpr:  // 属性访问模式
+				ProcessMemberExpression<T>(builder, memberExpr);
+				break;
+			case ParameterExpression paramExpr:  // 直接对象绑定模式
+				ValidateObjectBinding(typeof(T), typeof(Tar));
+				break;
+			default:
+				throw new ArgumentException("表达式必须为属性访问或对象绑定");
+		}
+
+		return builder.Build();
+	}
+
+	private static void ValidateObjectBinding(Type sourceType, Type targetType)
+	{
+		if (!targetType.IsAssignableFrom(sourceType))
+			throw new InvalidOperationException($"类型不兼容：{sourceType}无法转换为{targetType}");
+	}
+
+	private static void ProcessMemberExpression<T>(CompiledBindingPathBuilder builder, MemberExpression expr)
+	{
+		var propName = expr.Member.Name;
+		var propType = expr.Type;
 
 		var clrProp = new ClrPropertyInfo(
 			propName,
-			obj => ((T)obj).GetType().GetProperty(propName).GetValue(obj),
-			(obj, val) => ((T)obj).GetType().GetProperty(propName).SetValue(obj, val),
-			typeof(Tar)
+			obj => ((T)obj).GetType().GetProperty(propName)?.GetValue(obj),
+			(obj, val) => ((T)obj).GetType().GetProperty(propName)?.SetValue(obj, val),
+			propType
 		);
-		builder.Property(clrProp, PropertyInfoAccessorFactory.CreateInpcPropertyAccessor);
-		var path = builder.Build();
-		return path;
-		// var ans = new CompiledBindingExtension(path){
 
-		// };
-		// return ans;
+		builder.Property(clrProp, PropertyInfoAccessorFactory.CreateInpcPropertyAccessor);
 	}
+
+
+
+	// public static CompiledBindingPath pth<T, Tar>(
+	// 	Expression<Func<T, Tar>> propertySelector
+	// ){
+	// 	var builder = new CompiledBindingPathBuilder();
+	// 	var body = propertySelector.Body;
+
+	// 	// 处理类型转换表达式
+	// 	if (body is UnaryExpression unaryExpr && unaryExpr.NodeType == ExpressionType.Convert){
+	// 		body = unaryExpr.Operand;
+	// 	}
+
+	// 	if (!(body is MemberExpression memberExpr)){
+	// 		throw new ArgumentException("表达式必须为属性访问");
+	// 	}
+	// 	//var memberExpr = (MemberExpression)propertySelector.Body;
+	// 	var propName = memberExpr.Member.Name;
+
+	// 	var clrProp = new ClrPropertyInfo(
+	// 		propName,
+	// 		obj => ((T)obj).GetType().GetProperty(propName).GetValue(obj),
+	// 		(obj, val) => ((T)obj).GetType().GetProperty(propName).SetValue(obj, val),
+	// 		typeof(Tar)
+	// 	);
+	// 	builder.Property(clrProp, PropertyInfoAccessorFactory.CreateInpcPropertyAccessor);
+	// 	var path = builder.Build();
+	// 	return path;
+	// 	// var ans = new CompiledBindingExtension(path){
+
+	// 	// };
+	// 	// return ans;
+	// }
 
 
 }
