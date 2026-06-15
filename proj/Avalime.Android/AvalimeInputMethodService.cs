09aadb9ba6d852cd8077e38b85cab48f73ee8850
Tsinswreng <Tsinswreng@gmail.com>
2026-06-15 12:49:08 +0800

    試註冊安卓輸入法

---
+++ proj/Avalime.Android/AvalimeInputMethodService.cs
@@ -0,0 +1,74 @@
+using System.Diagnostics;
+using Android.App;
+using Android.Content;
+using Android.InputMethodServices;
+using Android.Runtime;
+using Android.Views;
+using Android.Views.InputMethods;
+using Android.Widget;
+
+namespace Avalime.Android;
+
+[Service(
+    Label = "Avalime",
+    Permission = global::Android.Manifest.Permission.BindInputMethod,
+    Exported = true)]
+[IntentFilter(new[] { "android.view.InputMethod" })]
+[MetaData("android.view.im", Resource = "@xml/ime_method")]
+public class AvalimeInputMethodService : InputMethodService
+{
+    public AvalimeInputMethodService() { }
+    public AvalimeInputMethodService(nint javaReference, JniHandleOwnership transfer)
+        : base(javaReference, transfer) { }
+
+    global::Android.Views.View? _inputView;
+
+    public override global::Android.Views.View OnCreateInputView()
+    {
+        Debug.WriteLine("[IME] OnCreateInputView");
+
+        if (_inputView != null)
+            return _inputView;
+
+        var layout = new LinearLayout(this)
+        {
+            Orientation = Orientation.Vertical
+        };
+        layout.LayoutParameters = new ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MatchParent,
+            ViewGroup.LayoutParams.WrapContent);
+
+        var testView = new TextView(this)
+        {
+            Text = "Avalime IME Prototype\nPress keys here",
+            TextSize = 24,
+            Gravity = GravityFlags.Center
+        };
+        testView.LayoutParameters = new ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MatchParent,
+            400);
+        testView.SetBackgroundColor(global::Android.Graphics.Color.DarkGray);
+        layout.AddView(testView);
+
+        _inputView = layout;
+        return _inputView;
+    }
+
+    public override void OnCreate()
+    {
+        base.OnCreate();
+        Debug.WriteLine("[IME] OnCreate");
+    }
+
+    public override void OnStartInputView(EditorInfo? info, bool restarting)
+    {
+        base.OnStartInputView(info, restarting);
+        Debug.WriteLine("[IME] OnStartInputView");
+    }
+
+    public override void OnFinishInputView(bool finishingInput)
+    {
+        base.OnFinishInputView(finishingInput);
+        Debug.WriteLine("[IME] OnFinishInputView");
+    }
+}
--- proj/Avalime.UI/Views/TopBar/ViewTopBar.cs
+++ proj/Avalime.UI/Views/TopBar/ViewTopBar.cs
@@ -15,30 +15,33 @@ public class ViewTopBar : AppViewBase<VmTopBar>
 
 	void Render(){
 		var root = new Grid{
-			ColumnDefinitions = new("Auto,*"),
+			ColumnDefinitions = new("*,Auto"),
 			RowDefinitions = new("Auto,Auto")
 		};
 
+		var candidates = new ViewCandidatesBar();
+		Grid.SetColumn(candidates, 0);
+		Grid.SetRowSpan(candidates, 2);
+
 		var btn = new Button{
 			Content = "連接 Rime",
-			HorizontalAlignment = HorizontalAlignment.Left
+			HorizontalAlignment = HorizontalAlignment.Right
 		};
 		btn.Click += (_, _) => Ctx?.ConnectRime();
+		Grid.SetColumn(btn, 1);
 
 		var status = new TextBlock{
 			VerticalAlignment = VerticalAlignment.Center,
-			Margin = new(12, 0, 0, 0)
+			HorizontalAlignment = HorizontalAlignment.Right,
+			Margin = new(0, 2, 4, 0)
 		};
 		Ctx!.Bind(status, x => x.Text, x => x.StatusText);
+		Grid.SetColumn(status, 1);
+		Grid.SetRow(status, 1);
 
-		var candidates = new ViewCandidatesBar();
-
+		root.Children.Add(candidates);
 		root.Children.Add(btn);
 		root.Children.Add(status);
-		root.Children.Add(candidates);
-		Grid.SetColumn(status, 1);
-		Grid.SetRow(candidates, 1);
-		Grid.SetColumnSpan(candidates, 2);
 
 		this.SetContent(root);
 	}
