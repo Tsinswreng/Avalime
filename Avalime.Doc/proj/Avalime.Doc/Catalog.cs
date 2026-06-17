bd1a6d53a1eb7a0c7282289bbf57a24d04d942da
Tsinswreng <Tsinswreng@gmail.com>
2026-06-17 13:07:36 +0800

    Avalime.Doc編譯通過

--- Avalime.Doc/proj/Avalime.Doc/Catalog.cs
+++ Avalime.Doc/proj/Avalime.Doc/Catalog.cs
@@ -3,23 +3,23 @@ namespace Avalime.Doc;
 using Tsinswreng.CsCore;
 
 [Doc($$"""
-#H[{{nameof(Avalime.Doc.Core.ImeStateFlow)}}][
+#H[{{nameof(Avalime.Doc.Core)}}][
 	ImeState 流程
 ]
 
-#H[{{nameof(Avalime.Doc.Rime.RimeSetupAndProcessor)}}][
+#H[{{nameof(Avalime.Doc.Rime)}}][
 	Rime 初始化與按鍵處理
 ]
 
-#H[{{nameof(Avalime.Doc.UI.KeyboardUi)}}][
+#H[{{nameof(Avalime.Doc.UI)}}][
 	UI 鍵盤與視圖組裝
 ]
 
-#H[{{nameof(Avalime.Doc.Android.AndroidImeHost)}}][
+#H[{{nameof(Avalime.Doc.Android)}}][
 	Android 輸入法宿主
 ]
 
-#H[{{nameof(Avalime.Doc.Windows.WindowsEntry)}}][
+#H[{{nameof(Avalime.Doc.Windows)}}][
 	Windows 啟動入口
 ]
 
