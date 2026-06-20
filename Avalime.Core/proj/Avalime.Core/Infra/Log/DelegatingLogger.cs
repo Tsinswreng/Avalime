b8ef4d1a60ea3ae5e64efef74dea36706afd1323
Tsinswreng <Tsinswreng@gmail.com>
2026-06-20 11:21:58 +0800

    改DelegatingLogger

--- Avalime.Core/proj/Avalime.Core/Infra/Log/DelegatingLogger.cs
+++ Avalime.Core/proj/Avalime.Core/Infra/Log/DelegatingLogger.cs
@@ -7,41 +7,41 @@ using Tsinswreng.CsCore;
 多態全局日志。適用于 [不想配依賴注入 又想據不同環境用不同日誌實現]旹。
 
 如
-public class AppLog:WrappedLog{...}
+public class AppLog:DelegatingLogger{...}
 
 class MainActivity{
 	OnCreate(){
-		AppLog.Inst.Inner = new AndroidLogcat();
+		AppLog.Inst.InnerLogger = new AndroidLogcat();
 	}
 }
 
 class WindowsEntry{
 	Main(){
-		AppLog.Inst.Inner = new ConsoleLog();
+		AppLog.Inst.InnerLogger = new ConsoleLog();
 	}
 }
 
 """)]
 public interface IDelegatingLogger : ILogger {
-	public ILogger Inner { get;set; }
+	public ILogger? InnerLogger { get;set; }
 }
 
 public class DelegatingLogger : IDelegatingLogger {
-	public ILogger Inner { get; set; }
-	public DelegatingLogger(ILogger Inner) {
-		this.Inner = Inner;
+	public ILogger? InnerLogger { get; set; }
+	public DelegatingLogger(ILogger InnerLogger) {
+		this.InnerLogger = InnerLogger;
 	}
 
 	public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) {
-		Inner.Log(logLevel, eventId, state, exception, formatter);
+		InnerLogger?.Log(logLevel, eventId, state, exception, formatter);
 	}
 
 	public bool IsEnabled(LogLevel logLevel) {
-		return Inner.IsEnabled(logLevel);
+		return InnerLogger?.IsEnabled(logLevel) ?? false;
 	}
 
 	public IDisposable? BeginScope<TState>(TState state) where TState : notnull {
-		return Inner.BeginScope(state);
+		return InnerLogger?.BeginScope(state);
 	}
 }
 
